2093f0872b563d61fb89b05f656de2cdf4756e54
leonardo060190 <leonardo-morruga@hotmail.com>
2023-11-19 18:17:15 -0300

    fazendo teste com jest

--- express-controle_tarefas/controller/StatusController.js
+++ express-controle_tarefas/controller/StatusController.js
@@ -6,67 +6,70 @@ module.exports = {
 
     //metodo para retonar todos os status cadastrados na tabela
     async index(req, res) {
-        await Status.sequelize.query(`SELECT * FROM status ORDER BY tipo `)
-            .then(([results, metadata]) => {
-               if (results.length > 0) {
-                    res.json(results);
-                } else {
-                    res.status(404).json({
-                        success: false,
-                        message: "Não há cadastros",
-                    });
-                }
-            }).catch((error) => {
-                res.status(500).json({
+        try {
+            const [results, metadata] = await Status.sequelize.query(`SELECT * FROM status ORDER BY tipo `)
+
+            if (results.length > 0) {
+                res.json(results);
+            } else {
+                res.status(404).json({
                     success: false,
-                    message: error.message,
+                    message: "Não há cadastros",
                 });
+            }
+        } catch (error) {
+            res.status(500).json({
+                success: false,
+                message: error.message,
             });
+        };
     },
     //////////////////////////////////////////////////////////////////////////////////////////////////
 
 
     //metodo que insere um novo status na tabela
     async store(req, res) {
-        // Verifique se o tipo já está cadastrado
-        const statusExistente = await Status.findOne({
-            where: { tipo: req.body.tipo }
-        });
-
-        if (statusExistente) {
-            return res.status(400).json({
-                success: false,
-                message: "Status já está cadastrado."
+        try {
+            // Verifique se o tipo já está cadastrado
+            const statusExistente = await Status.findOne({
+                where: { tipo: req.body.tipo }
             });
-        }
-        await Status.sequelize.query(
-            `INSERT INTO status (
+
+            if (statusExistente) {
+                return res.status(400).json({
+                    success: false,
+                    message: "Status já está cadastrado."
+                });
+            }
+            const [results, metadata] = await Status.sequelize.query(
+                `INSERT INTO status (
                 tipo,
                 created_at,
                 updated_at
             )
             VALUES (?, ?, ?)`,
-            {
-                replacements:
-                    [
-                        req.body.tipo,
-                        new Date(),
-                        new Date()
-                    ]
+                {
+                    replacements:
+                        [
+                            req.body.tipo,
+                            new Date(),
+                            new Date()
+                        ]
 
-            }
-        )
-            .then(([results, metadata]) => {
+                }
+            )
+            if (results) {
                 res.status(201).json({
                     success: true,
                     message: "Status cadastrado com sucesso",
                 });
-            }).catch((error) => {
-                res.status(500).json({
-                    success: false,
-                    message: error.message,
-                });
+            }
+        } catch (error) {
+            res.status(500).json({
+                success: false,
+                message: error.message,
             });
+        }
     },
     ////////////////////////////////////////////////////////////////////////////////////////////////
 
---
+++ express-controle_tarefas/status.test.js
@@ -0,0 +1,48 @@
+const Status = require('./models/statu');
+const sequelize = require('./dataBase/index');
+
+describe('StatusController', () => {
+    beforeEach(() => {
+        // Clear all mocks before each test
+        jest.clearAllMocks();
+    });
+
+    it('should return an array of status', async () => {
+        // Simulate existing status objects
+        const status1 = { id: 1, tipo: 'Em andamento' };
+        const status2 = { id: 2, tipo: 'Concluído' };
+
+        // Mock the Status model's findAndCountAll method using sequelize
+        jest.spyOn(Status, 'findAndCountAll').mockResolvedValue({
+            count: 2,
+            rows: [status1, status2],
+        });
+
+        // Import and call the StatusController's index method
+        const controller = require('./controller/statusController');
+        const response = await controller.index();
+
+        // Verify the response status code and body
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toEqual([status1, status2]);
+    });
+
+    it('should return 404 if there are no status records', async () => {
+        // Mock empty results for findAndCountAll using sequelize
+        jest.spyOn(Status, 'findAndCountAll').mockResolvedValue({
+            count: 0,
+            rows: [],
+        });
+
+        // Import and call the StatusController's index method
+        const controller = require('./controller/statusController');
+        const response = await controller.index();
+
+        // Verify the response status code and error message
+        expect(response.statusCode).toBe(404);
+        expect(response.body).toEqual({
+            success: false,
+            message: 'Não há cadastros',
+        });
+    });
+});
