c0e5d5a5f9e482a5407f6a899312d7609aa69dff
leonardo060190 <leonardo-morruga@hotmail.com>
2023-11-29 21:53:16 -0300

    criando o post no front

--- express-controle_tarefas/controller/TarefasController.js
+++ express-controle_tarefas/controller/TarefasController.js
@@ -98,6 +98,9 @@ module.exports = {
 
     //método para que insere uma nova tarefa na tabela
     async store(req, res) {
+        console.log("teste");
+        const teste = req.body.titulo;
+        console.log(teste);
         // Verifique se o titulo já está cadastrado
         const tarefasExistente = await Tarefas.findOne({
             where: { titulo: req.body.titulo }
--- react-controle_tarefas/src/components/project/TaskForm.jsx
+++ react-controle_tarefas/src/components/project/TaskForm.jsx
@@ -1,6 +1,6 @@
-// import { useEffect, useState } from 'react';
-// import {api } from '../../config/ConfigAxios';
-
+import { useState } from 'react';
+import { api } from '../../config/ConfigAxios';
+import { useForm } from 'react-hook-form';
 
 import PropTypes from 'prop-types';
 import Input from '../form/input/Input';
@@ -9,27 +9,82 @@ import SubmitButton from '../form/submitbuttom/SubmitButton';
 
 import styles from './TaskForm.module.css';
 
-function TaskForm ({ btnText }) {
+function TaskForm({ btnText }) {
 
+    const { register, handleSubmit, reset } = useForm();
+    const [setAviso] = useState("");
+
+    const salvar = async (campos) => {
+        try {
+            const response = await api.post('/tarefas', campos); {
+                console.log(response);
+                setAviso(`Livro cadastrado com sucesso!`);
+                // titulo: data.titulo,
+                // descricao: data.descricao,
+                // data_criacao: data.data_criacao,
+                // data_limite: data.data_limite,
+                // status_id: data.status_id
+            }
+
+            if (response.ok) {
+                // Handle success, e.g., redirect or show a success message
+                console.log('Task created successfully');
+                setAviso('Tarefa cadastrada com sucesso!');
+            } else {
+                // Handle errors, e.g., show an error message
+                console.error('Failed to create task');
+                setAviso('Erro... Tarefa não cadastrada!');
+            }
+        } catch (error) {
+            console.error('Error creating task:', error);
+            setAviso(`Erro... Tarefa não cadastrada! ${error}`);
+        }
+
+        // Clear the form fields for a new task
+        reset({ titulo: '', descricao: '', data_criacao: '', data_limite: '', status_id: '' });
+
+        // Hide the message after 5 seconds
+        setTimeout(() => {
+            setAviso("");
+        }, 5000);
+    };
 
     return (
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={handleSubmit(salvar)}>
             <Input
                 type="text"
                 text="Nome da Tarefa"
-                name="name"
+                name="titulo"
                 placeholder="Insira o nome da Tarefa"
+                {...register('titulo')}
             />
 
             <Input
                 type="text"
                 text="Descrição da Tarefa"
-                name="description"
+                name="descricao"
                 placeholder="Insira a descrição da Tarefa"
+                {...register('descricao')}
             />
+
+            <div className={styles.inputdate}>
+                <Input
+                    type="datetime-local"
+                    text="Data de Criação"
+                    name="data_criacao"
+                    {...register('data_criacao')}
+                />
+                <Input
+                    type="datetime-local"
+                    text="Data para Finalizar"
+                    name="data_limite"
+                    {...register('data_limite')}
+                />
+            </div>
             <Select
-                name="category_id"
+                name="status_id"
                 text="Selecione o Status"
+                {...register('status_id')}
 
             />
 
